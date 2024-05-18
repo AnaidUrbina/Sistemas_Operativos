9866f8eb1fc60fcacefc6949755e0b428d0cc9d4
AnaidUrbina <167812196+AnaidUrbina@users.noreply.github.com>
2024-05-18 13:58:37 -0600

    Add files via upload

---
+++ contar_palabras.py
@@ -0,0 +1,13 @@
+#funcion para contar palabras
+def contar_palabras (archivo):
+    with open (archivo, 'r') as file:
+        contenido=file.read()
+        palabras=contenido.split()
+        num_palabras=len(palabras)
+        return num_palabras
+    
+#Programa Principal
+archivo='Escritorio/palabras.txt'
+total_palabras=contar_palabras(archivo)
+print("El total de palabras es: ", total_palabras)
+    
